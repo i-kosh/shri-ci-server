e969f83cbe4bcbf42640b5f2d96d4455eca49dbb
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-11 22:58:36 +0300

    fix: ошибочки

--- src/utils/validateConfig.ts
+++ src/utils/validateConfig.ts
@@ -2,8 +2,8 @@ import { Config } from '../config/types'
 
 export default function (cfg: Config): void {
   if (!cfg.PORT) throw new Error('env PORT is mandatory')
-  if (!cfg.TOKEN) throw new Error('env PORT is mandatory')
-  if (!cfg.DB) throw new Error('env PORT is mandatory')
+  if (!cfg.TOKEN) throw new Error('env TOKEN is mandatory')
+  if (!cfg.DB) throw new Error('env DB is mandatory')
 
   if (cfg.NODE_ENV !== 'production' && cfg.NODE_ENV !== 'development') {
     throw new Error('env NODE_ENV not production nor development ')
