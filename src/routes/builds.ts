ec587750afb9d40a0e37d552e7eb7e7786b72b99
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-11 18:00:47 +0300

    fix: меняет название параметра

--- src/routes/builds.ts
+++ src/routes/builds.ts
@@ -6,7 +6,7 @@ router.get('/', (req, res, next) => {
   res.json({ lol: 'получение списка сборок' })
 })
 
-router.get('/:commitHash', (req, res, next) => {
+router.get('/:buildId', (req, res, next) => {
   res.json({ lol: 'получение информации о конкретной сборке' })
 })
 
@@ -14,7 +14,7 @@ router.post('/:commitHash', (req, res, next) => {
   res.json({ lol: 'добавление сборки в очередь' })
 })
 
-router.get('/:commitHash/logs', (req, res, next) => {
+router.get('/:buildId/logs', (req, res, next) => {
   res.json({ lol: 'получение логов билда (сплошной текст)' })
 })
 
