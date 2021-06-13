b37585d7b4df077f866804b111407ba79f73f616
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-13 19:23:28 +0300

    chore: removes dist

--- dist/middlewares/finalErrorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const handler = (err, req, res, next) => {
-    res.status(400).json({
-        text: 'Bad request',
-    });
-    next();
-};
-exports.default = handler;
