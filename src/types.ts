68d7b87037f4d9172101898cc8335653084e97e9
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-23 22:55:14 +0300

    feat: типы для ручек

---
+++ src/types.ts
@@ -0,0 +1,46 @@
+// GetBuild
+export interface Build {
+  id: string
+  configurationId: string
+  buildNumber: number
+  commitMessage: string
+  commitHash: string
+  branchName: string
+  authorName: string
+  status: string
+  start: string
+  duration: number
+}
+export type BuildID = string
+export type BuildResponse = Build
+
+// QueueBuild
+export type CommitHash = string
+export type QueueBuildResponse = undefined
+
+// BuildList
+export interface BuildListParams {
+  offset?: number
+  limit?: number
+}
+export type BuildListResponse = Build[]
+
+// BUildLog
+export type LogResponse = string
+
+// Settings Save
+export interface SettingsSaveRequest {
+  buildCommand: string
+  mainBranch?: string
+  period?: number
+  repoName: string
+}
+
+// SettingsGet
+export interface SettingsResponse {
+  id: string
+  repoName: string
+  buildCommand: string
+  mainBranch?: string
+  period?: number
+}
