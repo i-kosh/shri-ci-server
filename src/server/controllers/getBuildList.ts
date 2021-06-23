a86b858e4b0f302a5b4f7429cd214bc037dd98ee
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-24 00:38:47 +0300

    feat: роут принимает квери параметры

--- src/server/controllers/getBuildList.ts
+++ src/server/controllers/getBuildList.ts
@@ -1,9 +1,17 @@
 import buildModel from '../models/Build'
 import { RequestHandler } from 'express'
+import { BuildListParams } from '../../types'
 
 const handler: RequestHandler = async (req, res, next) => {
+  const { limit, offset }: BuildListParams = req.query
+
   try {
-    const response = await buildModel.getBuildList()
+    const response = await buildModel.getBuildList({
+      params: {
+        limit,
+        offset,
+      },
+    })
 
     res.json(response.data.data)
   } catch (error) {
