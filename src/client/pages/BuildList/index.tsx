85f3a9afad7a3431e530e6d393ce702f1f4e802e
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-24 00:40:33 +0300

    feat: отрисовка реальных данных

--- src/client/pages/BuildList/index.tsx
+++ src/client/pages/BuildList/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import { DefaultLayout } from '../../layouts/Default'
 import { Button } from '../../components/Button'
 import { ReactComponent as PlaySvg } from '../../assets/play.svg'
@@ -7,9 +7,43 @@ import { Modal } from '../../components/Modal'
 import { Box } from '../../components/Box'
 import { Input } from '../../components/Input'
 import './style.scss'
+import { useLazyFetchBuildsListQuery } from '../../store/buildsApi'
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import { selectSettings } from '../../store/settingsSlice'
+import { selectBuildsList, addBuildsList } from '../../store/buildsSlice'
+import type { Build } from '../../../types'
+
+const getStatus = (status: Build['status']): BuildCardProps['status'] => {
+  switch (status) {
+    case 'Success':
+      return 'success'
+    case 'Waiting':
+      return 'wait'
+    default:
+      return 'fail'
+  }
+}
 
 export const BuildList: FunctionComponent = () => {
+  const loadingLimit = 25
+  const dispatch = useAppDispatch()
+
+  const settings = useAppSelector(selectSettings)
+  const buildList = useAppSelector(selectBuildsList)
+
   const [isModalOpen, setModalOpen] = useState(false)
+  const [fetchBuilds, { data, isUninitialized }] = useLazyFetchBuildsListQuery()
+
+  useEffect(() => {
+    if (!buildList.length && isUninitialized)
+      fetchBuilds({ limit: loadingLimit })
+
+    if (data) {
+      dispatch(addBuildsList(data))
+    }
+  }, [data])
+
+  const noMoreBuilds = data && data.length < loadingLimit
 
   const settingsButton = (
     <>
@@ -42,84 +76,43 @@ export const BuildList: FunctionComponent = () => {
     </>
   )
 
-  const testData: BuildCardProps[] = [
-    {
-      author: 'Philip Kirkorov',
-      commitHash: '952e5567ffdfho38932u',
-      duration: '8742834',
-      mainBranch: 'main',
-      msg: 'Super cool UI kit for making websites that look like games',
-      number: '67289342',
-      startDate: '2021-04-17T10:20:30.931Z',
-      status: 'wait',
-      oneline: true,
-      selectable: true,
-    },
-    {
-      author: 'Philip Kirkorov',
-      commitHash: '952e5567ffdfho38932u',
-      duration: '8742834',
-      mainBranch: 'main',
-      msg: 'Super cool UI kit for making websites that look like games',
-      number: '67289342',
-      startDate: '2021-04-17T10:20:30.931Z',
-      status: 'success',
-      oneline: true,
-      selectable: true,
-    },
-    {
-      author: 'Philip Kirkorov',
-      commitHash: '952e5567ffdfho38932u',
-      duration: '8742834',
-      mainBranch: 'main',
-      msg: 'Super cool UI kit for making websites that look like games',
-      number: '342342342',
-      startDate: '2021-04-17T10:20:30.931Z',
-      status: 'fail',
-      oneline: true,
-      selectable: true,
-    },
-    {
-      author: 'Philip Kirkorov',
-      commitHash: '952e5567ffdfho38932u',
-      duration: '8742834',
-      mainBranch: 'main',
-      msg: 'Super cool UI kit for making websites that look like games',
-      number: '342342342',
-      startDate: '2021-04-17T10:20:30.931Z',
-      status: 'fail',
-      oneline: true,
-      selectable: true,
-    },
-    {
-      author: 'Philip Kirkorov',
-      commitHash: '952e5567ffdfho38932u',
-      duration: '8742834',
-      mainBranch: 'main',
-      msg: 'Super cool UI kit for making websites that look like games',
-      number: '67289342',
-      startDate: '2021-04-17T10:20:30.931Z',
-      status: 'wait',
-      oneline: true,
-      selectable: true,
-    },
-  ]
-
   return (
-    <DefaultLayout
-      addButtons={settingsButton}
-      title="philip1967/my-aw
-    esome-repo-with-long-long-long-repo-name"
-    >
+    <DefaultLayout addButtons={settingsButton} title={settings.reponame}>
       <ul className="build-list">
-        {testData.map((props, i) => (
-          <li className="build-list__item" key={i}>
-            <BuildCard {...props} path={`/build/${props.number}`}></BuildCard>
+        {buildList.map((build) => (
+          <li className="build-list__item" key={build.id}>
+            <BuildCard
+              author={build.authorName}
+              commitHash={build.commitHash}
+              duration={build.duration}
+              mainBranch={build.branchName}
+              msg={build.commitMessage}
+              number={`${build.buildNumber}`}
+              status={getStatus(build.status)}
+              startDate={build.start}
+              path={`/build/${build.id}`}
+              selectable
+              oneline
+            ></BuildCard>
           </li>
         ))}
       </ul>
 
-      <Button className="build-list__more ">Show more</Button>
+      {!noMoreBuilds && (
+        <Button
+          className="build-list__more"
+          nativeAttrs={{
+            onClick: () => {
+              fetchBuilds({
+                offset: buildList.length,
+                limit: loadingLimit,
+              })
+            },
+          }}
+        >
+          Show more
+        </Button>
+      )}
 
       <Modal open={isModalOpen}>
         <Box modal className="build-list__newbuild newbuild">
