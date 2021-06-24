7e1797c3475161c37d868221418755185906cb84
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-24 18:49:58 +0300

    feat: добавляет утилиту

---
+++ src/client/utils/extractError.ts
@@ -0,0 +1,26 @@
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query/react'
+import { SerializedError } from '@reduxjs/toolkit'
+import { ErrorResponse } from '../../types'
+
+type Error = FetchBaseQueryError | SerializedError | undefined
+
+interface ExtractedError {
+  message: string
+  status: number | string
+}
+
+export const extractError = (err: Error): ExtractedError => {
+  if (err && 'data' in err) {
+    const data = err.data as ErrorResponse
+
+    return {
+      status: err.status,
+      message: data.text,
+    }
+  }
+
+  return {
+    status: 400,
+    message: 'Error occured',
+  }
+}
