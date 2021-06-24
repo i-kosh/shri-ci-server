b99e2d2c13ce32e6cd1de8ef2d8f13bd0d30e46b
Igor Koshelev <igor.koshelev440@gmail.com>
2021-06-24 18:51:18 +0300

    feat: компонент для создания тостов

---
+++ src/client/components/ToastBox/index.tsx
@@ -0,0 +1,83 @@
+import React, {
+  FC,
+  createContext,
+  useState,
+  ReactNode,
+  useEffect,
+  useRef,
+} from 'react'
+import { createPortal } from 'react-dom'
+import { v4 as uuid } from 'uuid'
+import { Message, MessageProps } from '../Message'
+import './style.scss'
+
+interface Toast {
+  content?: ReactNode
+  msg?: {
+    text: ReactNode
+    props: MessageProps
+  }
+  key?: string
+}
+
+type ToastWithKey = Toast & { key: string }
+
+interface ToastsCtx {
+  add: (time: number, toast: Toast) => void
+}
+
+export const ToastsContext = createContext<ToastsCtx | undefined>(undefined)
+
+const dockId = 'toast-dock'
+
+export const ToastBox: FC = (props) => {
+  const { children } = props
+
+  return createPortal(
+    <div className="toast">{children}</div>,
+    document.querySelector(`#${dockId}`)!
+  )
+}
+
+export const ToastDock: FC = ({ children }) => {
+  const [dockList, setDockList] = useState<ToastWithKey[]>([])
+  const [ctx, updCtx] = useState<ToastsCtx>()
+  const ref = useRef(dockList)
+
+  useEffect(() => {
+    ref.current = dockList
+
+    updCtx({
+      add: (time, toast) => {
+        const toastClone = { ...toast }
+        if (!toastClone.key) toastClone.key = uuid()
+        const key = toastClone.key
+        const toastWithKey = toastClone as ToastWithKey
+
+        setDockList(dockList.concat(toastWithKey))
+
+        setTimeout(() => {
+          setDockList(ref.current?.filter((val) => val.key !== key))
+        }, time)
+      },
+    })
+  }, [dockList])
+
+  return (
+    <ToastsContext.Provider value={ctx}>
+      {children}
+
+      <div id={dockId}>
+        {dockList.map((toast) => (
+          <ToastBox key={toast.key}>
+            {toast.content ? (
+              toast.content
+            ) : (
+              <Message {...toast.msg?.props}>{toast.msg?.text}</Message>
+            )}
+          </ToastBox>
+        ))}
+      </div>
+    </ToastsContext.Provider>
+  )
+}
