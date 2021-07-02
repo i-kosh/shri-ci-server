2b56efac06bae50992dac028a2b32ae4bdcff307
Igor Koshelev <igor.koshelev440@gmail.com>
2021-07-02 14:37:37 +0300

    feat: заглушка для стилей и jest-dom

--- src/client/jest.config.ts
+++ src/client/jest.config.ts
@@ -4,7 +4,12 @@ const config: Config.InitialOptions = {
   name: 'client',
   displayName: 'client',
 
+  testEnvironment: 'jsdom',
   testMatch: ['**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jestSetup.ts'],
 }
 
 export default config
