6a23a48fc82c7332cbe4a2d28a6efc84f9ff76a0
Igor Koshelev <igor.koshelev440@gmail.com>
2021-07-03 00:54:55 +0300

    feat: добавляет fetch в глобальную область для тестов

--- src/client/jestSetup.ts
+++ src/client/jestSetup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom'
+import { Headers, Request, Response, fetch } from 'cross-fetch'
+
+globalThis.fetch = fetch
+globalThis.Response = Response
+globalThis.Request = Request
+globalThis.Headers = Headers
