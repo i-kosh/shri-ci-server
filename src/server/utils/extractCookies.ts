7025448d25b92a35a377460fdb9af62346faa10b
Igor Koshelev <igor.koshelev440@gmail.com>
2021-07-05 16:22:02 +0300

    feat: дополнительно поле флаг

--- src/server/utils/extractCookies.ts
+++ src/server/utils/extractCookies.ts
@@ -1,14 +1,23 @@
 import { Request } from 'express'
 import type { Cookie } from '@wdio/protocols'
 
-export type CookiesNames = 'mockApi'
+export type CookiesNames = 'testMockApi' | 'testNoSettings'
 
 export interface AvailableCookie extends Cookie {
   name: CookiesNames
 }
 
+interface CookiesExtended extends Partial<Record<CookiesNames, string>> {
+  haveTestCookies: boolean
+}
+
 export const extractCookies = (
   req: Request<any, any, any, any>
-): Partial<Record<CookiesNames, string>> => {
-  return req.cookies
+): CookiesExtended => {
+  return {
+    ...req.cookies,
+    haveTestCookies: Object.keys(req.cookies).find((val) =>
+      val.startsWith('test')
+    ),
+  }
 }
