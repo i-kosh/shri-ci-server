0bbaaedcdd1d0fec3e99113f524d3940a0e0b5e7
Igor Koshelev <igor.koshelev440@gmail.com>
2021-07-16 19:25:09 +0300

    fix: исправляет проблемы со сборкой
    Исправлет невероятно большой бандл
    Также объединяет все конфиги в один файл

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,132 @@
-import { resolve } from 'path'
-import { Configuration } from 'webpack'
 import type { Options } from 'ts-loader'
+import { resolve } from 'path'
+import { Configuration, DefinePlugin } from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
+import postcssPresetEnv from 'postcss-preset-env'
+import autoprefixer from 'autoprefixer'
+import { green, yellow } from 'colors'
+
+if (
+  process.env.NODE_ENV !== 'development' &&
+  process.env.NODE_ENV !== 'production'
+) {
+  process.env.NODE_ENV = 'production'
+}
+console.log(green(`NODE_ENV=${process.env.NODE_ENV}`))
+
+const isDev = process.env.NODE_ENV === 'development'
+
+if (isDev) {
+  console.log(yellow('Development build'))
+} else {
+  console.log(green('Production build'))
+}
 
 const dist = resolve(__dirname, 'dist')
 
-const common: Configuration = {
-  mode: 'development',
+// ### Client ###
+
+const client: Configuration = {
+  name: 'client',
+  mode: isDev ? 'development' : 'production',
+  devtool: isDev ? 'eval-source-map' : false,
+  target: 'web',
+  watchOptions: {
+    ignored: 'src/server/*',
+  },
+  entry: {
+    app: './src/client/index.tsx',
+    metrics: './src/client/metrics/index.ts',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  output: {
+    filename: 'static/[name].js',
+    path: dist,
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          name: 'vendors',
+          test: /node_modules/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/client/index.html',
+      publicPath: '/',
+      excludeChunks: ['metrics'],
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'static/[name].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/public/*.*',
+          to: resolve(dist, 'static/[name][ext]'),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer(), postcssPresetEnv()],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgo: false,
+            },
+          },
+          'url-loader',
+        ],
+      },
+    ],
+  },
+}
+
+// ### Server ###
+const serverCommonConfig: Configuration = {
+  mode: isDev ? 'development' : 'production',
   target: 'node',
   resolve: {
     extensions: ['.ts', '.js'],
@@ -16,12 +137,13 @@ const common: Configuration = {
     path: dist,
   },
   optimization: {
-    minimize: false,
+    minimize: !isDev,
   },
 }
 
 const server: Configuration = {
-  ...common,
+  ...serverCommonConfig,
+  name: 'server',
   entry: {
     server: './src/server/server.ts',
   },
@@ -41,7 +163,8 @@ const server: Configuration = {
 }
 
 const agent: Configuration = {
-  ...common,
+  ...serverCommonConfig,
+  name: 'agent',
   entry: {
     agent: './src/agent/index.ts',
   },
@@ -60,4 +183,4 @@ const agent: Configuration = {
   },
 }
 
-export default [server, agent]
+export default [client, server, agent]
